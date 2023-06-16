125fdfb880d26e9c69dae1342f55b1f3ab4594e4
Akash Sarkar <76092689+ADITYAFLAME4@users.noreply.github.com>
2023-06-16 23:01:09 +0530

    Added Files

---
+++ WhatsappAutomation.py
@@ -0,0 +1,4 @@
+# FIrst input your desired Ph no , then add your MSG and then change the time and run.. all must be in '' .
+
+import pywhatkit
+pywhatkit.sendwhatmsg('+910000000000', 'YOUR MESSEGE', 00,00 )
